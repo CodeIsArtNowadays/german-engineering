54aba8062551b73f14c4ca3e19ebda2d8431ad27
Arthur <vrthurrain@gmail.com>
2026-08-04 00:25:24 +0300

    llamaindex basic

--- llamaindex/main.py
+++ llamaindex/main.py
@@ -1,10 +1,10 @@
 import chromadb
 
-from llama_index.core import Settings, SimpleDirectoryReader, VectorStoreIndex
-from llama_index.vector_stores.chroma import ChromaVectorStore
+from llama_index.core import Settings, SimpleDirectoryReader, VectorStoreIndex, StorageContext
 from llama_index.llms.ollama import Ollama
 from llama_index.embeddings.ollama import OllamaEmbedding
 from llama_index.readers.file import PyMuPDFReader
+from llama_index.vector_stores.chroma import ChromaVectorStore
 
 # Settings
 Settings.llm = Ollama(
@@ -18,26 +18,20 @@ Settings.embed_model = OllamaEmbedding(
     model_name='all-minilm:33m',
     request_timeout=300,
 )
-# Load documents
-extractor = {'.pdf': PyMuPDFReader()}
-documents = SimpleDirectoryReader('data', file_extractor=extractor).load_data() # pyright: ignore
 
 # DB settins
 chroma_client = chromadb.PersistentClient(path='./vector_db')
 chroma_collection = chroma_client.get_or_create_collection(name='ragged')
+print(chroma_collection.count())
 vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
 
-# Add documents to context
-index = VectorStoreIndex.from_documents(documents, vector_store=vector_store)
 
-# Quering
-
-def query_handler(query: str):
+def query_handler(query: str, index):
     query_engine = index.as_query_engine()
     response = query_engine.query(query)
     print(response)
 
-def retrieve_and_synth_by_hand(query: str):
+def retrieve_and_synth_by_hand(query: str, index):
     retriever = index.as_retriever(similarity_top_k=2)
     nodes = retriever.retrieve(query)
 
@@ -49,7 +43,30 @@ def retrieve_and_synth_by_hand(query: str):
     response = synthesizer.synthesize(query, nodes=nodes)
     print(response)
 
-query = "Who is the current CFO of BrightPath Solutions?"
 
-retrieve_and_synth_by_hand(query)
+def get_creating_index(path_to_documents_dir: str):
+    # Load documents
+    extractor = {'.pdf': PyMuPDFReader()}
+    documents = SimpleDirectoryReader(path_to_documents_dir, file_extractor=extractor).load_data() # pyright: ignore
+    # Creating context
+    storage_context = StorageContext.from_defaults(
+        vector_store=vector_store
+    )
+    # Add documents to context
+    index = VectorStoreIndex.from_documents(documents, storage_context=storage_context)
+    storage_context.persist(persist_dir='./vector_db')
+    return index
 
+def get_retrieving_index():
+    return VectorStoreIndex.from_vector_store(vector_store=vector_store)
+
+
+def main():
+    # index = get_creating_index('data')
+    index = get_retrieving_index()
+
+    query = "When was BrightPath Solutions founded?"
+    query_handler(query, index)
+
+if __name__ == '__main__':
+    main()
